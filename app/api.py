a7a6bdd3992a1953217a86f46947acaefd716a04
Yauhenii <yauhenmardan@gmail.com>
2021-11-25 23:24:31 +0100

    minor fixes

--- app/api.py
+++ app/api.py
@@ -11,6 +11,6 @@ async def get_entity_by_index(index) -> Entity:
     return create_entity_by_index(index)
 
 
-@app.post("/entity/post/by-index/name={name},index={index},number={number},date={date}")
+@app.post("/entity/post/name={name}&index={index}&number={number}&date={date}")
 async def post_entity_by_index(name, index, number, date) -> Entity:
     return create_entity(name, index, number, date)
