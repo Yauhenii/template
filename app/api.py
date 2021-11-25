bc45319cc34b900d358d93f319eaa304a6c2515d
Yauhenii <yauhenmardan@gmail.com>
2021-11-25 23:13:01 +0100

    add post method

--- app/api.py
+++ app/api.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-from stub import create_entity_by_index
 
+from stub import create_entity_by_index, create_entity
 from model import Entity
 
 app = FastAPI()
 
 
 @app.get("/entity/get/by-index/{index}", response_model=Entity)
-async def get_entity_by_index(index):
-    return create_entity_by_index(int(index))
+async def get_entity_by_index(index) -> Entity:
+    return create_entity_by_index(index)
+
+
+@app.post("/entity/post/by-index/name={name},index={index},number={number},date={date}")
+async def post_entity_by_index(name, index, number, date) -> Entity:
+    return create_entity(name, index, number, date)
--- app/stub/factory/entity_factory.py
+++ app/stub/factory/entity_factory.py
@@ -3,6 +3,16 @@ from datetime import date
 from model import Entity
 
 
+def create_entity(name:str, index: int, number: float, date: date) -> Entity:
+    entity = Entity(
+        entity_name="entity" + str(index),
+        entity_index=index,
+        entity_number=number,
+        entity_date=date,
+    )
+    return entity
+
+
 def create_entity_by_index(index: int) -> Entity:
     entity = Entity(
         entity_name="entity" + str(index),
