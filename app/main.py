50c2bb72eedd310ae664b363e200415263e6dc93
Yauhenii <yauhenmardan@gmail.com>
2021-11-26 10:21:34 +0100

    add custom logger with colored output

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import uvicorn
 
-from config import settings 
+from config import settings
 
 if __name__ == "__main__":
     uvicorn.run(
         settings.uvicorn.app,
         host=settings.uvicorn.host,
         port=settings.uvicorn.port,
-        log_level=settings.uvicorn.log_level,
+        log_config=settings.logging.log_config_path,
         reload = settings.uvicorn.reload
     )
