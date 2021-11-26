378e90b59d558e2933ab76515070f5ac4f05f8ba
Yauhenii <yauhenmardan@gmail.com>
2021-11-26 10:44:56 +0100

    add logger info to apis

--- app/api.py
+++ app/api.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+import logging
 
 from stub import create_entity_by_index, create_entity
 from model import Entity
 
 app = FastAPI()
+logger = logging.getLogger('uvicorn.error')
 
 
 @app.get("/entity/get/by-index/{index}", response_model=Entity)
 async def get_entity_by_index(index) -> Entity:
-    return create_entity_by_index(index)
+    entity = create_entity_by_index(index)
+    logger.info(entity)
+    return entity
 
 
 @app.post("/entity/post/name={name}&index={index}&number={number}&date={date}")
 async def post_entity_by_index(name, index, number, date) -> Entity:
-    return create_entity(name, index, number, date)
+    entity =  create_entity(name, index, number, date)
+    logger.info(entity)
+    return entity
